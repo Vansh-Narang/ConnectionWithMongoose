1c47dd842570b1aea1325cf6c1d6efa2b6112c46
Vansh Narang <102366482+Vansh-Narang@users.noreply.github.com>
2023-06-28 23:06:33 +0530

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,9 @@
+   <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>,
+
+//In react js
